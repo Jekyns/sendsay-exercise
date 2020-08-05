2308e039a1af874a7970aef134a7f43d4b177907
Alexandr <Jekyns@yandex.ru>
2020-08-05 23:48:37 +0300

    changed input label to functionality component and fix linter error

--- src/components/Login/InputLabel.js
+++ src/components/Login/InputLabel.js
@@ -1,33 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 
-class InputLabel extends React.PureComponent {
-  state = {
-  };
-
-  // componentDidMount() {}
-  render() {
-    const { title, type, required, name, onChange, value, error } = this.props;
-    const titleClass = `label__head-title ${error ? 'errorText' : null}`;
-    const inputClass = `label__input-input ${error ? 'errorInput' : null}`;
-    return (
-      <label className="inputs__label">
-        <div className="label__head">
-          <span className={titleClass}>{title}</span>
-          {required ? null : <span className="label__head-optional">Опционально</span>}
-        </div>
-        <div className="label__input">
-          <input
-            className={inputClass}
-            type={type}
-            value={value}
-            name={name}
-            onChange={onChange}
-          />
-        </div>
-      </label>
-    );
-  }
+export default function InputLabe(props) {
+  const { title, type, required, name, onChange, value, error } = props;
+  const titleClass = `label__head-title ${error ? 'errorText' : null}`;
+  const inputClass = `label__input-input ${error ? 'errorInput' : null}`;
+  return (
+    <label className="inputs__label">
+      <div className="label__head">
+        <span className={titleClass}>{title}</span>
+        {required ? null : <span className="label__head-optional">Опционально</span>}
+      </div>
+      <div className="label__input">
+        <input
+          className={inputClass}
+          type={type}
+          value={value}
+          name={name}
+          onChange={onChange}
+        />
+      </div>
+    </label>
+  );
 }
 
-export default InputLabel;
+InputLabe.propTypes = {
+  title: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  error: PropTypes.bool.isRequired,
+  onChange: PropTypes.func,
+  required: PropTypes.bool.isRequired,
+  type: PropTypes.string.isRequired,
+};
+
+InputLabe.defaultProps = {
+  title: '',
+  name: '',
+  value: '',
+  error: false,
+  required: false,
+  type: 'text',
+};
