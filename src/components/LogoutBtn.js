4ed8fec138a8a2f9528834e0751cfc7050c7c47e
Alexandr <Jekyns@yandex.ru>
2020-08-05 23:43:47 +0300

    changed to functionallity component and fix linter error

--- src/components/LogoutBtn.js
+++ src/components/LogoutBtn.js
@@ -3,45 +3,36 @@ import PropTypes from 'prop-types';
 import cookie from 'react-cookies';
 import Sendsay from 'sendsay-api';
 import { connect } from 'react-redux';
-import { setUser, deleteUser } from '../store/user/actions';
+import { deleteUser } from '../store/user/actions';
 import logoutIcon from '../img/icons/logout.png';
 
-class LogoutBtn extends React.PureComponent {
-  deleteUser = () => {
-    this.props.deleteUser();
+function LogoutBtn(props) {
+  const deleteUser = () => {
+    props.deleteUser();
     cookie.remove('sendsay_session');
     const sendsay = new Sendsay({});
     sendsay.request({
       action: 'logout',
       session: cookie.load('sendsay_session'),
     });
-  }
+  };
 
-  render() {
-    return (
-      <button className="logout" onClick={this.deleteUser}>
-        <span className="logout-span">
-          Выйти
+  return (
+    <button className="logout" onClick={deleteUser}>
+      <span className="logout-span">
+        Выйти
         </span>
-        <img alt="logout icon" className="logout-img" src={logoutIcon}/>
-      </button>
-    );
-  }
+      <img alt="logout icon" className="logout-img" src={logoutIcon} />
+    </button>
+  );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.userStore.user,
-  };
-};
-
 const mapDispatchToProps = {
-  setUser,
   deleteUser,
 };
 
 const enchancer = connect(
-  mapStateToProps,
+  undefined,
   mapDispatchToProps,
 );
 
